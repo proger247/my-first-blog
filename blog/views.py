6c9adb046d46d1e0b357b3cd73b8a35168591d62
proger247 <comelkov2012@gmail.com>
2019-09-29 02:42:06 +0500

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Postment
 from django.utils import timezone
+
 
 
 def post_list(request):
     posts = Postment.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})   # {'posts': posts} параметр передаваемый шаблону у
+
+def post_detail(request, pk):
+    post = get_object_or_404(Postment, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})

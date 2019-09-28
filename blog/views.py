a8d2ee0460c54d87c0597fcb6cfdf56e6eead5c1
proger247 <comelkov2012@gmail.com>
2019-09-28 18:37:14 +0500

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Postment
+from django.utils import timezone
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Postment.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})   # {'posts': posts} параметр передаваемый шаблону у
